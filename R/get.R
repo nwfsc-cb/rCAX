a8fdf792ec41be3856928ebf6fa822f473f437b4
Eli Holmes <eli.holmes@noaa.gov>
2022-11-30 12:21:56 -0800

    minor

--- R/get.R
+++ R/get.R
@@ -2,3 +2,39 @@ updateddata<-httr::content(
   httr::GET('https://apex.psmfc.org/akfin/data_marts/akmp/baranof_avg_sst?start_date=20220117&end_date=20221231'), 
   type = "application/json") %>% 
   bind_rows
+
+GET(
+  "https://api.streamnet.org/api/v1/users",
+  authenticate("user", "passwd")
+)
+
+apikey <- GET(
+  "https://api.streamnet.org/api/v1/users",
+  query = list(email = "email", password = "password")
+  )
+
+httr::content(
+httr::GET('https://api.streamnet.org/api/v1/ca/tables'),
+type = "application/json")
+
+
+secret <- jsonlite::base64_enc( readChar("Callahan_token.txt",nchars=1e6) ) 
+# Get token from API
+req <- httr::POST("https://apex.psmfc.org/akfin/data_marts/oauth/token",
+                  httr::add_headers(
+                    "Authorization" = paste("Basic", gsub("\n", "", secret)),
+                    "Content-Type" = "application/x-www-form-urlencoded;charset=UTF-8"
+                  ),
+                  body = "grant_type=client_credentials"
+);
+
+#  Create authentication error message
+httr::stop_for_status(req, "Something broke.")
+token <- paste("Bearer", httr::content(req)$access_token)
+
+#Actual API call  
+age<-fromJSON(content(
+  GET('https://apex.psmfc.org/akfin/data_marts/akm/get_lls_age_view?startyear=2019&endyear=2020',
+      add_headers(Authorization = token)), 
+  as="text")) %>% 
+  bind_rows
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CAX
+
 repo for accessing CAX
+
+documentation: https://www.streamnet.org/resources/exchange-tools/rest-api-documentation/
+
+https://www.dataquest.io/blog/r-api-tutorial/
