640994db3ee2dbbc9f2335f4ff92d1ef414419a1
Eli Holmes <eli.holmes@noaa.gov>
2022-12-06 11:18:45 -0800

    add presmolt base
    #2

--- R/rcax_presmolt.R
+++ R/rcax_presmolt.R
@@ -1,29 +1,38 @@
-#' Return NOSA base table
-#' 
+#' Return Presmolt Abundance base table
 #'  
 #' @export
 #' @template info
 #' @template tableargs
 #' @template basetablefuns
-#' @seealso `rcax_nosa_xport()`, `rcax_table_query()`, `rcax_filter()`, `rcax_key()`
+#' @seealso `rcax_presmolt_xport()`, `rcax_table_query()`, `rcax_filter()`, `rcax_key()`
 #' @examples 
 #' # return popid of first record
 #' # Note the part after $ is case sensitive
-#' id <- rcax_nosa(qlist=list(limit=1))$popid
+#' id <- rcax_presmolt(qlist=list(limit=1))$popid
 #' 
-#' a <- rcax_nosa(flist=list(popid=id))
-#' a <- a[, c("popid", "spawningyear", "nosaij", "nosaej")]
-#' head(a)
+#' # Make a plot of one population
+#' a <- rcax_presmolt(flist=list(popid=id))
+#' library(ggplot2)
+#' ggplot(a, aes(x=surveyyear, y=abundance, color=waterbody)) +
+#'   geom_line()
+#' 
+#' # First 3 columns
+#' rcax_presmolt(qlist=list(limit=3))[,1:3]
 #' 
 #' # to print the first 5 column names
-#' rcax_nosa(type="colnames")[1:5]
+#' rcax_presmolt(type="colnames")[1:5]
 #' 
-rcax_nosa <- function(
-    tablename = "NOSA",
+#' # to print the first 5 column names as they appear
+#' # in Excel files downloaded from https://www.streamnet.org/data/hli/
+#' tab <- rcax_presmolt(qlist=list(limit=1))
+#' colnames(tab)[1:5]
+#' 
+rcax_presmolt <- function(
+    tablename = "PresmoltAbundance",
     flist = NULL,
     qlist = NULL, 
     cols = NULL,
-    sortcols = c("spawningyear", "popid"),
+    sortcols = c("surveyyear", "popid"),
     type = c("data.frame", "colnames"), 
     GETargs = list(table_id = NULL, recordloc = "records", key = NULL, parse = TRUE), ...) {
   # Error checking. Most happens in rcax_table_query.
@@ -34,7 +43,7 @@ rcax_nosa <- function(
      cat("popid appears in both flist and qlist. Ignoring the value in qlist.\n")
     qlist$popid <- NULL
   }
-
+  
   # API call and table filtering and sorting
   rcax_table_query(
     tablename = tablename, 
