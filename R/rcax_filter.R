7c12a3a653ccb1f0434650e346c9b15a262d7da6
Eli Holmes <eli.holmes@noaa.gov>
2022-12-13 15:05:34 -0800

    add some more filter url code

--- R/rcax_filter.R
+++ R/rcax_filter.R
@@ -12,6 +12,13 @@
 #' @details 
 #' The property names in `x` are the column names in a table being returned by `rcax_table_query()`. 
 #' Note the values are not case sensitive. So a value of `Spring` will return the data with `Spring` and `spring`.
+#' 
+#' {crul} properly formats the filter given the JSON. For reference, here
+#' is a url example. Note that in the filter double quotes are required.
+#' A single quote will not filter. 
+#' ```
+#' httr::GET('https://api.streamnet.org/api/v1/ca.json?table_id=4EF09E86-2AA8-4C98-A983-A272C2C2C7E3&XApiKey=C4F5F084-EBE3-4ED6-8AF1-125EC54E6E52&filter=[{"field":"esu_dps","value":"Salmon,%20coho%20(Oregon%20Coast%20ESU)","type":"string"}]')
+#' ```
 #' @examples 
 #' rcax_filter(list(commonpopname="GRCAT"))
 #' rcax_filter(list(popid=c(7,8)))
