2aef1bc430ecd21f47f54bdf04cbf1fee83d2d41
Eli Holmes <eli.holmes@noaa.gov>
2022-12-13 15:06:32 -0800

    merge rcax_hli and rcax_hli_xport

--- R/rcax_hli_xport.R
+++ R/rcax_hli_xport.R
@@ -1,27 +1,28 @@
-#' Return tables from CAP Fish HLIs Tabular Query
+#' Return fish HLI tables from Coordinated Assessments data eXchange 
 #'  
 #' @export
-#' @param hli The HLI short name
+#' @param hli The HLI short name: NOSA, SAR, PNI, PreSmolt, JuvOut, RperS
+#' @param tabletype "xport" (default) or "base". XPort has additional useful metadata.
 #' @template info
 #' @template tableargs
 #' @template xportfuns
-#' @seealso `rcax_hli()`, `rcax_table_query()`, `rcax_filter()`
+#' @seealso `rcax_table_query()`, `rcax_filter()`, `rcax_table_name`
 #' @examples 
 #' # return NMFS_PopID of second record
 #' # Note the part after $ is case sensitive
-#' id <- rcax_hli_xport("NOSA", qlist=list(limit=2))$nmfs_popid[2]
+#' id <- rcax_hli("NOSA", qlist=list(limit=2))$nmfs_popid[2]
 #' 
-#' a <- rcax_hli_xport("NOSA",
+#' a <- rcax_hli("NOSA",
 #'          flist=list(nmfs_popid=id),
 #'          cols=c("nmfs_popid", "spawningyear", "tsaej", "nosaej")
 #'          )
 #' head(a)
 #' 
 #' # First 3 columns
-#' rcax_hli_xport("NOSA", qlist=list(limit=3))[,1:3]
+#' rcax_hli("NOSA", qlist=list(limit=3))[,1:3]
 #' 
 #' # to print the first column names and definitions
-#' head(rcax_hli_xport("NOSA", type="colnames"))
+#' head(rcax_hli("NOSA", type="colnames"))
 #' 
 rcax_hli_xport <- function(
     hli = c("NOSA", "SAR", "PNI", "PreSmolt", "JuvOut", "RperS"),
@@ -30,16 +31,18 @@ rcax_hli_xport <- function(
     cols = NULL,
     sortcols = NULL,
     type = c("data.frame", "colnames"), 
+    tabletype = c("xport", "base"),
     GETargs = list(table_id = NULL, recordloc = "records", key = NULL, parse = TRUE), ...) {
   hli <- match.arg(hli)
   type <- match.arg(type)
+  tabletype <- match.arg(tabletype)
   # Error checking. Most happens in rcax_table_query.
   assert_is(flist, 'list')
   assert_is(qlist, 'list')
   
-  tablename <- rcax_table_name(hli, type="xport")
-  if(is.null(sortcols)) sortcols <- rcax_table_sortcols(hli, type="xport")
-  if(is.null(cols)) cols <- rcax_table_cols(hli, type="xport")
+  tablename <- rcax_table_name(hli, tabletype = tabletype)
+  if(is.null(sortcols)) sortcols <- rcax_table_sortcols(hli, type=tabletype)
+  if(is.null(cols)) cols <- rcax_table_cols(hli, type=tabletype)
   
   if(("popid" %in% names(flist)) & ("popid" %in% names(qlist))){
      cat("popid appears in both flist and qlist. Ignoring the value in qlist.\n")
